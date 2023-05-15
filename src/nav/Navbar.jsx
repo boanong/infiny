eada18c1ca9fe3d7fa692b038e4e46c743efa9d3
Boanong Joshua <boanongjoshua@gmail.com>
2023-05-15 23:31:15 +0100

    Design navbar

---
+++ src/nav/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+      <div className="navbar__logo">Logo</div>
+      <div className="navbar__links">
+        <a href="#" className="navbar__link">Contact Us</a>
+        <a href="#" className="navbar__link">About Us</a>
+        <a href="#" className="navbar__link">Courses</a>
+      </div>
+      <div className="navbar__buttons">
+        <button className="navbar__button">Login</button>
+        <button className="navbar__button">Sign Up</button>
+      </div> 
+</nav>
+
+    </div>
+  )
+}
+
+export default Navbar
